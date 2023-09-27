aec7144e0ef75c41baf6f5a05cce649a23f10c48
Cesar Humberto Grifaldo Garcia <cesar.grifaldo@elektra.com.mx>
2023-09-27 13:59:42 -0600

    Initial Commit

---
+++ ViperArchitectureAlterMoviesUITests/ViperArchitectureAlterMoviesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ViperArchitectureAlterMoviesUITestsLaunchTests.swift
+//  ViperArchitectureAlterMoviesUITests
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 27/09/23.
+//
+
+import XCTest
+
+final class ViperArchitectureAlterMoviesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
